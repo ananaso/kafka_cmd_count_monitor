2c4abd470ba38b1cedd18ec665b839ec99a8937d
Alden Davidson <adavidson+github@protonmail.ch>
2021-04-12 16:16:09 -0700

    added initial check for cmd packets

--- src/kafka_cmd_count_monitor.py
+++ src/kafka_cmd_count_monitor.py
@@ -12,10 +12,9 @@ BOOTSTRAP = 'esnode01.has.com:9092'
 #BOOTSTRAP = 'esnode02.has.com:9092' # alt bootstrap
 
 consumer = KafkaConsumer(INTOPIC, bootstrap_servers = BOOTSTRAP)
-
 logfile = sys.stdout
-cmd_valid_count = None
 
+cmd_valid_count = None
 for msg in consumer:
     try:
         data = json.loads(msg.value)
@@ -24,23 +23,23 @@ for msg in consumer:
         decoded_message = message['decoded']
 
         # we only want to parse messages that apply to our tests
-        if ttctype != TTC_TYPE_TLM:
-            continue
+        if ttctype == TTC_TYPE_TLM:
+            # isolate the only cmd counts we care about
+            latest_cmd_valid_count = 0
+            for key, value in decoded_message.items():
+                if fnmatch.fnmatch(key, '*CMD_VALID_COUNT'):
+                    print(f'valid: {key} = {value}')
+                    latest_cmd_valid_count += value
 
-        # isolate the only cmd counts we care about
-        latest_cmd_valid_count = 0
-        for key, value in decoded_message.items():
-            if fnmatch.fnmatch(key, '*CMD_VALID_COUNT'):
-                print(f'valid: {key} = {value}')
-                latest_cmd_valid_count += value
-
-        # i.e. if we're initializing
-        if cmd_valid_count is None:
-            cmd_valid_count = latest_cmd_valid_count
-        elif cmd_valid_count != latest_cmd_valid_count:
-            print(f"WARNING: latest command count ({latest_cmd_valid_count}) \
-                    doesn't matched stored command count ({cmd_valid_count})", 
-                    logfile)
+            # i.e. if we're initializing
+            if cmd_valid_count is None:
+                cmd_valid_count = latest_cmd_valid_count
+            elif cmd_valid_count != latest_cmd_valid_count:
+                print(f"WARNING: latest command count ({latest_cmd_valid_count}) \
+                        doesn't matched stored command count ({cmd_valid_count})", 
+                        logfile)
+        elif ttctype == TTC_TYPE_CMD:
+            print(f'got command packet: {decoded_message}')
 
     except Exception as err:
         print(f'Failed to decode: {msg.value} {err}', logfile)
