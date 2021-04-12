4be740e8ce2d1e8115922524eb0e161c65c5f58f
Alden Davidson <adavidson+github@protonmail.ch>
2021-04-12 16:13:47 -0700

    added count init and comparison

--- src/kafka_cmd_count_monitor.py
+++ src/kafka_cmd_count_monitor.py
@@ -6,14 +6,14 @@ import sys
 from kafka import KafkaConsumer
 
 INTOPIC = 'cosmos-out'
-TTC_TYPE = 'COSMOS_SIM_TELEMETRY'
+TTC_TYPE_TLM = 'COSMOS_SIM_TELEMETRY'
+TTC_TYPE_CMD = 'COSMOS_SIM_COMMAND'
 BOOTSTRAP = 'esnode01.has.com:9092'
 #BOOTSTRAP = 'esnode02.has.com:9092' # alt bootstrap
 
 consumer = KafkaConsumer(INTOPIC, bootstrap_servers = BOOTSTRAP)
 
 logfile = sys.stdout
-
 cmd_valid_count = None
 
 for msg in consumer:
@@ -22,16 +22,25 @@ for msg in consumer:
         message = json.loads(data['message'])
         ttctype = message['zeek']['ttc_type']
         decoded_message = message['decoded']
-        print(f'\n---decoded---{decoded_message}\n')
 
         # we only want to parse messages that apply to our tests
-        if ttctype != TTC_TYPE:
-            print(f'wrong ttc type: {ttctype}')
+        if ttctype != TTC_TYPE_TLM:
             continue
 
+        # isolate the only cmd counts we care about
+        latest_cmd_valid_count = 0
         for key, value in decoded_message.items():
             if fnmatch.fnmatch(key, '*CMD_VALID_COUNT'):
                 print(f'valid: {key} = {value}')
+                latest_cmd_valid_count += value
+
+        # i.e. if we're initializing
+        if cmd_valid_count is None:
+            cmd_valid_count = latest_cmd_valid_count
+        elif cmd_valid_count != latest_cmd_valid_count:
+            print(f"WARNING: latest command count ({latest_cmd_valid_count}) \
+                    doesn't matched stored command count ({cmd_valid_count})", 
+                    logfile)
 
     except Exception as err:
         print(f'Failed to decode: {msg.value} {err}', logfile)
